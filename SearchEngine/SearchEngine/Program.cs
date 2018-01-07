589e01e9de06a7aef65171f5fad61370c4f91ab9
Jiale Zhu <jialzh@microsoft.com>
2018-01-07 23:24:14 +0800

    Add OCR, going to start pic split

--- SearchEngine/SearchEngine/Program.cs
+++ SearchEngine/SearchEngine/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Drawing;
 using System.Text;
 using System.Net;
 using System.IO;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace SearchEngine
 {
@@ -18,12 +21,8 @@ namespace SearchEngine
 
         const string OCRAccessKey = "c214a5cca92446d993d80ba493e2e64a";
 
-        // Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
-        // search APIs.  In the future, regional endpoints may be available.  If you
-        // encounter unexpected authorization errors, double-check this value against
-        // the endpoint for your Bing Web search instance in your Azure dashboard.
-        const string uriBase = "https://api.cognitive.microsoft.com/bing/v7.0/search";
-        
+        const string workspace = @"D:\Workspace\AnswerApp\";
+
         // Used to return search results including relevant headers
         struct SearchResult
         {
@@ -35,26 +34,46 @@ namespace SearchEngine
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+            // Get the path and filename to process from the user.
+            Console.WriteLine("Optical Character Recognition:");
+            Console.Write("Enter the path to an image with text you wish to read: ");
+            string imageFilePath = Console.ReadLine();
+
+            // Execute the REST API call.
+            MakeOCRRequest(imageFilePath);
+
+            Console.WriteLine("\nPlease wait a moment for the results to appear. Then, press Enter to exit...\n");
+            Console.ReadLine();
+
+            Console.Write("\nPress Enter to exit ");
+            Console.ReadLine();
+        }
+
+        static void SplitPic(string picname)
+        {
+            Bitmap pic = (Bitmap)Image.FromFile(picname);
+        }
+
+        static int SearchCounter(string searchTerm)
+        {
             if (BingSearchAccessKey.Length == 32)
             {
-                string searchTerm = "美国";
                 Console.WriteLine("Searching the Web for: " + searchTerm);
                 SearchResult result = BingWebSearch(searchTerm);
-                
+
                 Console.WriteLine("\nResult count:\n");
                 int start = result.jsonResult.IndexOf("totalEstimatedMatches") + 24;
                 int end = result.jsonResult.IndexOf("value") - 3;
                 int count = int.Parse(result.jsonResult.Substring(start, end - start));
                 Console.WriteLine(count);
                 //Console.WriteLine(JsonPrettyPrint(result.jsonResult));
+                return count;
             }
             else
             {
                 Console.WriteLine("Invalid Bing Search API subscription key!");
+                return -1;
             }
-
-            Console.Write("\nPress Enter to exit ");
-            Console.ReadLine();
         }
 
         /// <summary>
@@ -62,6 +81,12 @@ namespace SearchEngine
         /// </summary>
         static SearchResult BingWebSearch(string searchQuery)
         {
+            // Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
+            // search APIs.  In the future, regional endpoints may be available.  If you
+            // encounter unexpected authorization errors, double-check this value against
+            // the endpoint for your Bing Web search instance in your Azure dashboard.
+            const string uriBase = "https://api.cognitive.microsoft.com/bing/v7.0/search";
+
             // Construct the URI of the search request
             var uriQuery = uriBase + "?q=" + Uri.EscapeDataString(searchQuery);
 
@@ -88,7 +113,68 @@ namespace SearchEngine
             return searchResult;
         }
 
-        /*
+        /// <summary>
+        /// Gets the text visible in the specified image file by using the Computer Vision REST API.
+        /// </summary>
+        /// <param name="imageFilePath">The image file.</param>
+        static async void MakeOCRRequest(string imageFilePath)
+        {
+            // Replace or verify the region.
+            //
+            // You must use the same region in your REST API call as you used to obtain your subscription keys.
+            // For example, if you obtained your subscription keys from the westus region, replace 
+            // "westcentralus" in the URI below with "westus".
+            //
+            // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+            // a free trial subscription key, you should not need to change this region.
+            const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr";
+
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", OCRAccessKey);
+
+            // Request parameters.
+            string requestParameters = "language=unk&detectOrientation=true";
+
+            // Assemble the URI for the REST API Call.
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            // Request body. Posts a locally stored JPEG image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json" and "multipart/form-data".
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                // Execute the REST API call.
+                response = await client.PostAsync(uri, content);
+
+                // Get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+
+                // Display the JSON response.
+                Console.WriteLine("\nResponse:\n");
+                Console.WriteLine(JsonPrettyPrint(contentString));
+            }
+        }
+        
+        /// <summary>
+        /// Returns the contents of the specified file as a byte array.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to read.</param>
+        /// <returns>The byte array of the image data.</returns>
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
         /// <summary>
         /// Formats the given JSON string by adding line breaks and indents.
         /// </summary>
@@ -104,9 +190,8 @@ namespace SearchEngine
             StringBuilder sb = new StringBuilder();
             bool quote = false;
             bool ignore = false;
-            char last = ' ';
             int offset = 0;
-            int indentLength = 2;
+            int indentLength = 3;
 
             foreach (char ch in json)
             {
@@ -115,16 +200,13 @@ namespace SearchEngine
                     case '"':
                         if (!ignore) quote = !quote;
                         break;
-                    case '\\':
-                        if (quote && last != '\\') ignore = true;
+                    case '\'':
+                        if (quote) ignore = !ignore;
                         break;
                 }
 
                 if (quote)
-                {
                     sb.Append(ch);
-                    if (last == '\\' && ignore) ignore = false;
-                }
                 else
                 {
                     switch (ch)
@@ -151,15 +233,13 @@ namespace SearchEngine
                             sb.Append(' ');
                             break;
                         default:
-                            if (quote || ch != ' ') sb.Append(ch);
+                            if (ch != ' ') sb.Append(ch);
                             break;
                     }
                 }
-                last = ch;
             }
 
             return sb.ToString().Trim();
         }
     }
-    */
 }
